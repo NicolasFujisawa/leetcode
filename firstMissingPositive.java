e2efef8216f911f5daa1d33aa8188f8ec1394bb1
NicolasFujisawa <nicolas.fujisawa@fatec.sp.gov.br>
2020-09-30 22:56:51 -0300

    add firstMissingPositive

---
+++ firstMissingPositive.java
@@ -0,0 +1,31 @@
+package sol;
+
+
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Solution {
+    private static final Logger LOGGER = Logger.getLogger( Solution.class.getName() );
+
+    public int firstMissingPositive(int[] nums) {
+        int answer = 1;
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] < 1 || nums[i] > nums.length || nums[i] == nums[nums[i] -1]) {
+                i++;
+                continue;
+            }
+            int temp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = temp;
+        }
+        LOGGER.log(Level.INFO, "{0}", Arrays.toString(nums));
+        for(int num : nums) {
+            if(answer == num) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
